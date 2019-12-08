9a92b30d6a056e0a6ffbd70f7224a6133729c03e
nathanael mosaku <37617151+Chidi2K18@users.noreply.github.com>
2019-12-08 11:03:24 +0000

    Create Text Adventure.cpp

---
+++ Text Adventure.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+using namespace std;
+void pathToCarnage();
+
+
+char path;
+char continuePTC;
+
+void pathChanger(char path)
+{
+    cout << "==================================================================" << endl;
+    cout << "==================================================================" << endl;
+    cout << "The routes that you have availabale to you are below" << endl;
+    cout << "" << endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));//this creates a time delay
+    cout << "Naruto Learns Flying thundergod (press F)" << endl;
+    cout << "" << endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));//this creates a time delay
+    cout << "Naruto learns Adamantine sealing chains (press A)" << endl;
+     cout << "" << endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));//this creates a time delay
+    cout << "Naruto's Path to carnage (press C)" << endl;
+    cout << "" << endl;
+    cout << "Make your choice now" << endl;
+cin >> path;
+if (path == 'C')
+{
+		pathToCarnage();//(path);
+	}
+}
+
+void pathToCarnage()
+{
+    cout << "===========================================================================================================================================================" << endl;
+    cout << "===========================================================================================================================================================" << endl;
+    cout << ""<< endl;
+    cout << "Act 1" << endl;
+    cout << "The Beginning" << endl;
+    cout << "" << endl;
+cout << "16 years ago the hidden leaf village was attacked by a demon known as the 9 tailed fox and this demon rampaged throughout the village causing untold amounts of destruction and loss of life. The village was left on the brink of destruction until the leader of the village, a ninja known throughout the ninja world as the fourth hokage(literally meaning 4th fire shadow) faught back against the beast and eventually defeating it, unfortunatle he was dealt a mortal wound and was on the verge of death. Fearing that the beast would one day come back to seek vengance against the village he decided to seal the demon within his newborn child believing that his child would one day master its power and become the savior of the world. As his final decree his master's teacher(the third hokage) was to reassume his post as the villages leader and to protect his son as he would grow as an orphan, the third hokage agreed. His final act in this world was to name his newborn child Naruto Uzumaki (using his mother's surname as using his own would put his child in danger) and promise to watch over him forever also telling him that his life would be hard but he has faith in him./n/n" << endl;
+cout << "" << endl;
+cout << "===========================================================================================================================================================" << endl;
+cout << "===========================================================================================================================================================" << endl;
+cout << "" << endl;
+std::this_thread::sleep_for(std::chrono::seconds(30));//this creates a time delay
+cout << "Naruto grew up alone as an orphan child, the third hokage opted to support him by giving him a monthly allowance to fend for himself. Naruto ended up growing up without knowing parental love or love of anykind as the villagers dispise him, they view him as the demon that killed so many villagers. Naruto was now 12 years old and grew up ostrisised with a chip on his shoulder as he was entirely unaware of the fact that he was the host to a demon from his perspective he was hated without reason and grew up without friends as parents didn't allow their children to assosiate with him. The third hokage decreed that nobody was to speak of that fated day 12 years ago and that they were forbidden from telling Naruto that a demon dwells within him as well as their children. Naruto eventually grew an intense desire for attention as people often ignored him and this caused naruto to act out and play pranks on the villagers, the scale of his pranks grew larger as he got older to the point where he would deface the villages national monument the hokage stone faces (think mount rushmore).n/n/";
+cout << "" << endl;
+cout << "" <<endl;
+std::this_thread::sleep_for(std::chrono::seconds(30));//this creates a time delay
+cout << "Naruto aspired to be hokage one day as he felt that if he became the leader of teh village he would have earned the respect of the villagers and people would acknowledge him, to this end he entered the ninja academy as the hokage is the strongest ninja in the village. He then failed the graduation exam 3 times.n/n/";
+cout <<""<< endl;
+cout << "" << endl;
+cout << "===========================================================================================================================================================" << endl;
+cout << "===========================================================================================================================================================" << endl;
+cout << "" << endl;
+std::this_thread::sleep_for(std::chrono::seconds(5));//this creates a time delay
+cout << "The HEROS journey or the path of the TYRANT" << endl;
+cout << "Naruto was unnable to pass because he could not perfrom an advanced technique known as the body flicker which allowed a ninja to increase their movement speed to such an extent that they would disappear from human eyes and appear somewhere else. This technique is treated as standard practice for ninja and Naruto was the only student in his class that could not perform this technique which gave his classmates room to ridicule him on top of their negative bias towards him(instilled from their parents). His teacher Iruka had lost faith in his ability to pass some time ago and this caused Naruto to become an outcast in the class, Iruka told Naruto to give up as he would never become a ninja and refused to teach him any further stating that if he did not pass the next exam that he would be expelled from the academy. There were other students in the academy that had failed the exam more times than Naruto had and yet they were not threatened with expulsion and Naruto knew this, knowing that everyone had some kind of bias against him created a hole in his heart and it was being filled with hatred towards his village?, no humanity for being able to treat someone so poorly without reason. Determined to pass the exam Naruto began to train and practice that technique with every waking moment, one evening he was approached by a ninja going by the name Mizuki and he told Naruto that there was more than one way to pass the exam. Exited by the prospect of another method of passing he asked what it was Mizuki told him that if he demonstrated use of a more powerful version of that technique that he would graduate at the top of his class. Mizuki then told him that the technique he needed to learn could be found on a scrool in the hokage's mansion also telling him that the third hokage liked to keep the most powerful techniques for himself so he would be helping people if he were to retrieve it.n/n/";
+cout << "" << endl;
+cout << "" << endl;
+std::this_thread::sleep_for(std::chrono::seconds(30));//this creates a time delay
+cout << "Naurto stole the scrool and ran away with it into the forests within the village and began to train, Mizuki later requested a meeting with the hokage and told him that he had seen Naruto running off with the scrool but he couldn't see where Naruto had gone saying that he had used clones to hide himself and escaped. The hokage declared a state of emergency for the ninja of the village as the scrool he had taken contained very powerful techniques as well as forbidden techniques that if mastered could bring the ninja world to its knees. Mizuki orchestrated this situation to allow him to find Naruto, kill him and take the scrool for himself. Naruto in the forest had been practicing the technique he was told about. Mizuki having a good idea of where Naruto had gone was the first ninja to find him, Mizuki prepared a large shuriken and hurled it towards Naruto hoping to kill him, Naurto noticed the shuriken and dodged it while protecting the scrool. Once he regained his composure he looked towards the direction the shuriken came from only to find a hail of regular sized shuriken coming towards him which he barely managed to dodge and one had managed to embed itself in his leg which hampered his movements. Naurto entered a state of panic as this was his first battle so he looked around frantically for a means of escape, to no avail he quickly turned around only to find his teacher Iruka watching the fight unfold from a high treebranch, Naurto desperately called for his teacher's help and to his dismay Iruka turned his back to him and said 'why would I help a demon child like you?' and left. It was in that moment that Naruto's heart was filled with hatred and rage (both triggers to summon the demons power), Naruto started to radiate a dark red energy with such force that the ground beneath him began to erode forming a crater with him as the epicenter.n/n/";
+cout << "" << endl;
+cout << "" << endl;
+std::this_thread::sleep_for(std::chrono::seconds(30));//this creates a time delay
+cout << "Mizuki was visibly affected by this energy and felt sick to his core as he looked around him the tree leaved began to wilter and fall it was at this moment Mizuki knew what he was sensing and exclamed 'this energy is death itself!.' Mizuki immediately ran away as fast as he could only to find Naurto suddenly sitting crosslegged  on a tree branch in front of him. Mizuki ran in a differnt direction to the same effect this continued 3 more times until the fourth he saw Naruto standing on the nearest tree branch in front of him and he noticed Naurto was enveloped in a blood red aura with glowing slit eyes(similar to a snakes). Mizuki turned and as he could see the direction he wanted to go in Naruto was right in front of him and said in a demonic voice 'try as you might there is no escape', Mizuki gained a confidence boost from effectivly staring death in the face and made a clone he kicked what he thought was Naruto but it disappeared and Naruto was stood on the ground directly beneath the tree branch he was on it was at this moment Mizuki realised he kicked an afterimage and a look of horror befell his face. Mizuki's clone as if expressing his deepest wishes ran away as fast as it could, Mizuki watched and wanted to do the same but the confidence he had was gone and he was now paralysed with fear Naurto appeared within Mizuki's gaze and made eye contact, stretched out his hand and the clone erupted into flames. Naurto the proceeded to say 'that is your fate' is an emotionless tone as Miziuki dropped to his knees and accepted his fate Naruto appeared behind him clasped his head with both hands and Incinirated it, ripped his body in two and burned that as well.n/n/";
+cout << "" << endl;
+cout << "" << endl;
+std::this_thread::sleep_for(std::chrono::seconds(25));//this creates a time delay
+cout << "Within his inner psykie Naruto was speaking to the demon as the demon said that what he had just felt was but a mere fraction of its power and offered Naruto more to which he agreed knowing it would cost his humanity as he had now chosen to dissasoiate himself from his vile species. Naruto broke the seal and asked the demon to help him exact his vengance on humanity, the demon agreed and said that he would be unable to grant Naruto more power as he was currently his body could not handle more that what he had given already telling Naruto to get stronger as he would have to in order to achieve their shared goal. Naruto was already familiar with the inner workings of the technique he saught after in the scrool however he had seen other powerful techniques which would be helpful to learn he withdrew the aura from his hands as it erroded whatever he touched. He had to fight back the aura from his hands as he learned a technique known as the necromancer which allowed him to revive the recently dead as shadow warriors to do his bidding, the fox was pleased with this technique being learned and this caused the aura to grow over his hand destroying the scrool.n/n/";
+cout << "" << endl;
+cout << "" << endl;
+std::this_thread::sleep_for(std::chrono::seconds(25));//this creates a time delay
+cout << "End of first act" << endl;
+cout << "Would you like to continue?" << endl;
+cin >> continuePTC;
+if (continuePTC == 'Y'){
+
+    cout <<""<< endl;
+    cout << "" << endl;
+    cout << "===========================================================================================================================================================" << endl;
+    cout << "===========================================================================================================================================================" << endl;
+    cout << "Act 2 begins" << endl;
+    cout << "THE DEMON CHILD" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    std::this_thread::sleep_for(std::chrono::seconds(25));//this creates a time delay
+    cout << "A year has passed since the day Naruto had killed for the first time and since then Naruto has been declared as a rogue ninja from the hidden leaf village, the thrid hokage has assigned the highest priority mission in the village to be the capture and safe return of Naruto and this is because the demon that dwells within him is classed as one of the 9 supreme grade demons in existance and its purpose as well as Naruto's is to become a weapon for the village. During this time period Naruto has been hunted relentlessly by all 5 of the great ninja villages's highest tiered ninja the Jounins and the Anbu black ops units. Naruto had been trying to learn his techniques whilst on the move or on the lookout for more ninja aiming to capture him. He and the demon eventually devised a plan that would allow Naruto to train whilst being on the move and that was to allow the demon to take control over his body while he wants to train which causes his psykie to dwell in the space created by the seal it is in that space where he practices his techniques and he need not think about maneuvering his body. Once Naruto is done trainig for the day they swap places again and Naruto takes over while the demon acts as a sensory boost for Naruto as they share the same senses, this method however does not negate the need for Naruto's body to rest and eat. From time to time Naruto would have to make camp in order to rest and eat, on occasion someone would attempt to capture him while he slept at that moment the demon would take over and teleport naruto away(this was the stronger technique he was promised). This was the highest level of use for this technique as usually users would have to place their hands on a surface or person to create a seal to teleport to however with the use of the demons energy he is able to place a seal anywhere within a 2km radius of himself(this is the range at which the demons energy can affect at Naruto's current level of strength).n/n/"<< endl;
+    }
+}
+
+/*void flyingThunderGod(char path)
+{
+
+}
+
+void Adamantine(char path)
+{
+
+}*/
+
+int main()
+{
+
+    cout << "Welcome to my Naruto interactive text adventure" << endl;
+    cout << "In the routes that you will soon be able to choose from the context is that naruto learns different techniques from the scrool of sealing from te beginning of the story" << endl;
+    cout << "Please choose the route that you would like to embark on" << endl;
+
+pathChanger(path);
+}
