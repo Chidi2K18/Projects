ccbbc48bd07ef19b2f40b6f35af0cbab36687da0
nathanael mosaku <37617151+Chidi2K18@users.noreply.github.com>
2019-12-09 16:42:36 +0000

    Update mathsquiz.cpp
    moved to work in progress as
    the random number generator isnt working as intended
    the randomizer for the array isnt working as intended

--- mathsquiz.cpp
+++ mathsquiz.cpp
@@ -2,12 +2,12 @@
 #include <time.h> 
 #include <string>
 using namespace std;
-void easyMode(modeChoice);
+void easyMode();
 char modeChoice;
 int y;
 int x;
 string mathOperator[2] = { "+" , "-" };
-int randomIndex[2] = {1,2};
+int randomIndex[2] = { 1,2 };
 int question;
 int numOfQuestions = 1;
 int answer;
@@ -15,25 +15,25 @@ bool correct;
 int correctCounter = 0;
 int correctAnswerPercentage = correctCounter / numOfQuestions * 100;
 
-void shuffle(int randomIndex[], int len){
-  int temp =0;
-  int random = 0;
-  
-  for(int i = 0; i < len; i++){
-    random = rand() % len;
-    temp = randomIndex[i];
-    randomIndex[i] = randomIndex[random];
-    randomIndex[random] = temp;
-  }
+void shuffle(int randomIndex[], int len) {
+	int temp = 0;
+	int random = 0;
+
+	for (int i = 0; i < len; i++) {
+		random = rand() % len;
+		temp = randomIndex[i];
+		randomIndex[i] = randomIndex[random];
+		randomIndex[random] = temp;
+	}
 }
 
-void modechanger(char modeChoice){
-	
+void modechanger(char modeChoice) {
+
 	cout << "Before the game starts you have been gifted the option of changing the mode in which you play this game" << endl;
 	cout << "Your options are as follows: " << endl;
 	cout << "Easy mode where you only have to answer questions using 2 digit numbers" << endl;
-    cout << "Normal mode where you will have to answer questions using 2 and 3 digit numbers" << endl;
-    cout << "Hard mode where you will have to answer questions using up to 4 digit numbers" << endl;
+	cout << "Normal mode where you will have to answer questions using 2 and 3 digit numbers" << endl;
+	cout << "Hard mode where you will have to answer questions using up to 4 digit numbers" << endl;
 	cout << "===========================================================" << endl;
 	cout << "===========================================================" << endl;
 	cout << "'E' for Easy " << endl;
@@ -42,41 +42,42 @@ void modechanger(char modeChoice){
 	cout << "Make your choice now" << endl;
 	cin >> modeChoice;
 
-	if (modeChoice == 'H')
+	/*if (modeChoice == 'H')
 	{
-		hardMode(modeChoice);
-	} 
+		hardMode();
+	}
 	else if (modeChoice == 'N')
 	{
-        normalMode(modeChoice);
+		normalMode();
 	}
-	else if (modeChoice == 'E')
+	else */
+	if (modeChoice == 'E')
 	{
-		easyMode(modeChoice);
+		easyMode();
 	}
 }
 
-void normalMode (char modeChoice)
+/*void normalMode()
 {
-    srand(time(NULL)); //set random seed
+	srand(time(NULL)); //set random seed
 	int y = rand() % 100 + 1; //get random number between 1 and 100
-    srand(time(NULL)); //set random seed
+	srand(time(NULL)); //set random seed
 	int x = rand() % 100 + 1; //get random number between 1 and 100
 }
 
-void hardMode (char modeChoice)
+void hardMode()
 {
-    srand(time(NULL)); //set random seed
+	srand(time(NULL)); //set random seed
 	int y = rand() % 1000 + 1; //get random number between 1 and 1000
-    srand(time(NULL)); //set random seed
+	srand(time(NULL)); //set random seed
 	int x = rand() % 100 + 1; //get random number between 1 and 1000
-}
+}*/
 
-int main(){
-    modechanger(modeChoice);
-    return 0;
+int main() {
+	modechanger(modeChoice);
+	return 0;
 }
-void easyMode(char modeChoice)
+void easyMode()
 {
 	while (numOfQuestions != 10)
 	{
@@ -84,8 +85,8 @@ void easyMode(char modeChoice)
 		int y = rand() % 10 + 1; //get random number between 1 and 10
 		srand(time(NULL)); //set random seed
 		int x = rand() % 10 + 1; //get random number between 1 and 10
-		shuffle(randomIndex, 2);
-		if (mathOperator[randomIndex[1]] == mathOperator[0]) // if random array index picker is equal to '+' or array element 0
+		shuffle(randomIndex, 5);
+		if (mathOperator[randomIndex[0]] == mathOperator[0]) // if random array index picker is equal to '+' or array element 0
 		{
 			question = y + x; // question is set to this
 
@@ -133,7 +134,7 @@ void easyMode(char modeChoice)
 				}
 			}
 		}
-		else if (mathOperator[randomIndex[1]] == mathOperator[1]) // if random array index picker is equal to '-' or array element 1
+		else if (mathOperator[randomIndex[0]] == mathOperator[1]) // if random array index picker is equal to '-' or array element 1
 		{
 			question = y - x;
 
